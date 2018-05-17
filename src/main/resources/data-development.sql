e937ead974fcbf6f0e88a1a32bd9b0b41b8b2de3
Tom Keller <tomlouiskeller@gmail.com>
2018-05-17 21:32:20 +0200

    Added the creation of a recipe into the data.sql file

--- src/main/resources/data-development.sql
+++ src/main/resources/data-development.sql
@@ -1,3 +1,17 @@
 -- data.sql will be executed after this file
 
 INSERT INTO category (name) VALUES ('Swiss');
+
+
+INSERT INTO nutritional_info
+(text)
+VALUES ('You will become obese');
+
+INSERT INTO instruction
+(text)
+VALUES ('Just do it');
+
+INSERT INTO recipe
+(cooking_duration, difficulty, image, preparation_duration, servings, source, title, url, instruction_id, nutritional_info_id)
+VALUES
+  (30, 'HARD', null, 30, 4, 'www.food.com', 'Rösti', 'www.food.com/roesti', 1, 1);
