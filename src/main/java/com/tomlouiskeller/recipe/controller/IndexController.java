c2d309f9d6a908a930b9dc99e9c264e987cca14b
Tom Keller <tomlouiskeller@gmail.com>
2018-05-18 15:09:16 +0200

    Added Controller method and template to display all recipes and another method to display all recipes under 30 minutes

--- src/main/java/com/tomlouiskeller/recipe/controller/IndexController.java
+++ src/main/java/com/tomlouiskeller/recipe/controller/IndexController.java
@@ -1,43 +1,32 @@
 package com.tomlouiskeller.recipe.controller;
 
 
-import com.tomlouiskeller.recipe.domain.Category;
 import com.tomlouiskeller.recipe.domain.Recipe;
-import com.tomlouiskeller.recipe.repository.CategoryRepository;
-import com.tomlouiskeller.recipe.repository.ProductRepository;
 import com.tomlouiskeller.recipe.repository.RecipeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
     private RecipeRepository recipeRepository;
-    private CategoryRepository categoryRepository;
-    private ProductRepository productRepository;
 
-    @Autowired
-    public IndexController(RecipeRepository recipeRepository, CategoryRepository categoryRepository, ProductRepository productRepository) {
+    public IndexController(RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
-        this.categoryRepository = categoryRepository;
-        this.productRepository = productRepository;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        Optional<Recipe> recipe = recipeRepository.findByTitle("Rösti");
-        Optional<Category> category = categoryRepository.findByName("German");
-
-        System.out.println("Recipe is " + recipe.get().getTitle() + " Id: " + recipe.get().getId());
-        System.out.println("Category is " + category.get().getName() + " Id: " + category.get().getId());
-
-        Optional<Recipe> recipe2 = recipeRepository.findByPreparationDurationPlusCookingDurationIsLessThanEqual(60);
-        System.out.println("Recipe that is made in less then 60 min: " + recipe2.get().getTitle() + " Id: " + recipe.get().getId());
-
-        return "index";
+    public String getAllRecipes(Model model){
+        Iterable<Recipe> recipes = recipeRepository.findAll();
+        model.addAttribute("recipes", recipes);
+        return "listRecipes";
     }
 
+    @RequestMapping({"/quickRecipes"})
+    public String getQuickRecipes(Model model){
+        Iterable<Recipe> quickRecipes = recipeRepository.findByPreparationDurationPlusCookingDurationIsLessThanEqual(30);
+        model.addAttribute("recipes", quickRecipes);
+        return "listRecipes";
+    }
 }
