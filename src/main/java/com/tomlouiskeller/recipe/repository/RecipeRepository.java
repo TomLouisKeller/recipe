42df52901df045398e200862e9137b912cb4113b
Tom Keller <tomlouiskeller@gmail.com>
2018-05-18 15:02:16 +0200

    Cleaned up RecipeRepository and added a TODO.

--- src/main/java/com/tomlouiskeller/recipe/repository/RecipeRepository.java
+++ src/main/java/com/tomlouiskeller/recipe/repository/RecipeRepository.java
@@ -6,20 +6,14 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
+// TODO: Get Ingredients sorted by id or a separate field
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
     Optional<Recipe> findByTitle(String title);
-    Optional<Recipe> findByPreparationDurationIsGreaterThanEqual(Integer duration);
-    Optional<Recipe> findByPreparationDurationIsLessThanEqual(Integer duration);
-    Optional<Recipe> findByCookingDurationIsGreaterThanEqual(Integer duration);
-    Optional<Recipe> findByCookingDurationIsLessThanEqual(Integer duration);
-
-
-    // TODO: These will return a list!
-    @Query("select r from Recipe r where (r.preparationDuration + r.cookingDuration) >= ?1")
-    Optional<Recipe> findByPreparationDurationPlusCookingDurationIsGreaterThanEqual(Integer duration);
 
     @Query("select r from Recipe r where (r.preparationDuration + r.cookingDuration) <= ?1")
-    Optional<Recipe> findByPreparationDurationPlusCookingDurationIsLessThanEqual(Integer duration);
+    Iterable<Recipe> findByPreparationDurationPlusCookingDurationIsLessThanEqual(Integer duration);
 
+    @Query("select r from Recipe r where (r.preparationDuration + r.cookingDuration) >= ?1")
+    Iterable<Recipe> findByPreparationDurationPlusCookingDurationIsGreaterThanEqual(Integer duration);
 }
